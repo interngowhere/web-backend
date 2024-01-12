ccc89b241b45ba5365ab5c10af3d2ad743b0c712
Ming Jun <evan.zhangmingjun@gmail.com>
2024-01-13 00:51:00 +0800

    feat: add middleware for improved url handling

--- internal/middleware/middleware.go
+++ internal/middleware/middleware.go
@@ -12,6 +12,8 @@ import (
 func Setup(r chi.Router) {
 	r.Use(middleware.Logger)
 	r.Use(middleware.Recoverer)
+	r.Use(middleware.CleanPath)
+	r.Use(middleware.RedirectSlashes)
 	r.Use(middleware.Heartbeat("/ping"))
 	r.Use(httprate.LimitByIP(120, time.Minute))
 }
