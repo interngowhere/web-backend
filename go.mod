8aa042700a201b6fd333743aa511e80b56bd8902
Ming Jun <evan.zhangmingjun@gmail.com>
2024-01-22 13:10:43 +0800

    feat: add cors middleware

--- go.mod
+++ go.mod
@@ -21,6 +21,7 @@ require (
 	github.com/apparentlymart/go-textseg/v13 v13.0.0 // indirect
 	github.com/cespare/xxhash/v2 v2.1.2 // indirect
 	github.com/decred/dcrd/dcrec/secp256k1/v4 v4.2.0 // indirect
+	github.com/go-chi/cors v1.2.1
 	github.com/go-openapi/inflect v0.19.0 // indirect
 	github.com/goccy/go-json v0.10.2 // indirect
 	github.com/google/go-cmp v0.5.6 // indirect
--- internal/middleware/middleware.go
+++ internal/middleware/middleware.go
@@ -1,10 +1,12 @@
 package middleware
 
 import (
+	"os"
 	"time"
 
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
+	"github.com/go-chi/cors"
 	"github.com/go-chi/httprate"
 	"github.com/go-chi/jwtauth/v5"
 	jwt "github.com/interngowhere/web-backend/internal/auth"
@@ -19,4 +21,11 @@ func Setup(r chi.Router) {
 	r.Use(middleware.Heartbeat("/ping"))
 	r.Use(httprate.LimitByIP(120, time.Minute))
 	r.Use(jwtauth.Verifier(jwt.TokenAuth))
+	r.Use(cors.Handler(cors.Options{
+		AllowedOrigins: []string{os.Getenv("PROD_ORIGIN"), os.Getenv("DEV_ORIGIN")},
+		AllowedMethods: []string{"GET", "POST", "PUT", "DELETE"},
+		AllowedHeaders: []string{"Accept", "Authorization", "Content-Type", "X-CSRF-Token"},
+		ExposedHeaders: []string{"Link"},
+		MaxAge:         300,
+	}))
 }
