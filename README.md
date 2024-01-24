fd9138749fed4c9623067392d839847a491a11a9
Ming Jun <evan.zhangmingjun@gmail.com>
2024-01-24 17:17:58 +0800

    refactor: seeding can now be done by running `go run cmd/seed/main.go`

--- README.md
+++ README.md
@@ -4,6 +4,26 @@
 
 ### Local Development
 
+**Environment variables**
+
+Set the environmental variables as follow
+| Key | Value | Remarks |
+|--------------|--------------------------------------|-----------------------------------------------------------------------|
+| DB_DIALECT | postgres | |
+| DB_HOST | database | `database` is the name of the golang server service in docker-compsoe |
+| DB_PORT | 5432 | |
+| DB_USER | | e.g. user |
+| DB_NAME | | e.g. postgres |
+| DB_PASSWORD | | e.g. mysecretpassword |
+| SERVER_PORT | 8000 | |
+| SERVER_HOST | server | `server` is the name of the golang server service in docker-compsoe |
+| JWT_SIGKEY | | Private key used to sign the JWT |
+| JWT_VALIDITY | | Time in minutes for which the token is valid |
+| DEV_ORIGIN | http://localhost:8000 | CORS allowed origin |
+| PROD_ORIGIN | https://interngowhere.netlify.app | CORS allowed origin |
+
+**Docker**
+
 To run docker containers locally, run
 
 ```
@@ -13,6 +33,28 @@ docker-compose -f docker-compose-dev.yml up -d
 This will spin up docker containers for the backend go server, PostgreSQL database, and nginx reverse proxy respectively.
 
 In dev environment, nginx is configured to listen on port 80. In production environment, nginx is configured to handle TLS/SSL encryption for HTTPS traffic and will listen on port 443.
+
+**Seeding the database**
+
+As the database port 5432 is also exposed on localhost in dev environment, you can simply run the following to seed the database.
+
+```
+go run cmd/seed/main.go
+```
+
+Alternatively, you may run the command inside the docker container with the backend server.
+
+This will get you shell access in the docker container:
+
+```
+docker exec -it server sh
+```
+
+You can then run the same command directly inside the container:
+
+```
+go run cmd/seed/main.go
+```
 
 ### Commit messages
 
--- cmd/seed/main.go
+++ cmd/seed/main.go
@@ -12,6 +12,91 @@ import (
 	_ "github.com/lib/pq"
 )
 
+// Data
+var SeedUser1 = ent.User{
+	Email:     "aiken@gmail.com",
+	FirstName: "Aiken",
+	LastName:  "Tan",
+	Username:  "AT",
+}
+
+var SeedUser2 = ent.User{
+	Email:     "dueet@gmail.com",
+	FirstName: "Dueet",
+	LastName:  "Tan",
+	Username:  "DT",
+}
+
+var SeedTopic1 = ent.Topic{
+	Title:            "Test Topic",
+	Slug:             "test-topic",
+	Description:      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+	ShortDescription: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+}
+
+var SeedTopic2 = ent.Topic{
+	Title:            "GovTech",
+	Slug:             "govtech",
+	Description:      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+	ShortDescription: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+}
+
+var SeedTopic3 = ent.Topic{
+	Title:            "Hackathon",
+	Slug:             "hackathon",
+	Description:      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+	ShortDescription: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+}
+
+var SeedThread1 = ent.Thread{
+	Title:       "This is a test thread",
+	Slug:        "this-is-a-test-thread",
+	Description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+}
+
+var SeedThread2 = ent.Thread{
+	Title:       "GovTech interview tips",
+	Slug:        "govtech-interview-tips",
+	Description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+}
+
+var SeedThread3 = ent.Thread{
+	Title:       "What is the culture like at GovTech?",
+	Slug:        "what-is-the-culture-like-at-govtech",
+	Description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+}
+
+var SeedThread4 = ent.Thread{
+	Title:       "Beginner's guide to hackathons",
+	Slug:        "beginners-guide-to-hackathons",
+	Description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+}
+
+var SeedComment1 = ent.Comment{
+	Content: "This is a test comment",
+}
+
+var SeedComment2 = ent.Comment{
+	Content: "This is a reply to the test comment",
+}
+
+var SeedComment3 = ent.Comment{
+	Content: "This is another reply to the test comment",
+}
+
+var SeedTag1 = ent.Tag{
+	TagName: "Behavioural Interview",
+}
+
+var SeedTag2 = ent.Tag{
+	TagName: "Online Assessment",
+}
+
+var SeedTag3 = ent.Tag{
+	TagName: "Leetcode",
+}
+
+// Seed function
 func main() {
 	// Setup database
 	db, err := ent.Open(
