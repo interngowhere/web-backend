b24268bd57ef92c6f2ca735c9e4205f8b6dc0ea6
Ming Jun <evan.zhangmingjun@gmail.com>
2024-01-24 16:48:22 +0800

    fix: incorrect hostname in server log

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -27,7 +27,7 @@ func main() {
 	database.Init()
 	defer database.Client.Close()
 
-	log.Println("Listening on port 8000 at http://localhost:8000")
+	log.Println(fmt.Printf("Listening on port 8000 at http://%s:8000\n", os.Getenv("SERVER_HOST")) )
 
 	log.Fatalln(http.ListenAndServe(fmt.Sprintf("%s:8000", os.Getenv("SERVER_HOST")), r))
 }
